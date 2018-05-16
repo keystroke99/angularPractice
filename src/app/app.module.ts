91912aa814ef9d2930558cbc0fdaa75c718b996b
Rohini Kumar D <rohin@indoztechsol.in>
2018-05-16 09:38:28 +0530

    added NavBAr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes, Router } from'@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DatevalidationComponent } from './components/datevalidation/datevalidation.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  {path: 'datevalidation', component: DatevalidationComponent},
+  {path: 'home', component: AppComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    DatevalidationComponent
+    DatevalidationComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes, 
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
